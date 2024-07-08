cde8a4d44a930d8a1871b92d8c1e8d01a6c896b0
R SUNILKUMAR <147584902+Sunilkumargouda@users.noreply.github.com>
2024-07-08 12:47:43 +0530

    update the files for code deploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull sunilkumarr097550/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sunilkumarr097550/simple-python-flask-app:latest
+docker run -d -p 5000:5000 sunilkumarr097550/simple-python-flask-app:latest 
